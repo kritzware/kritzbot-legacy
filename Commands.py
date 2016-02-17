aad4df814e79d989b88b281c81002ba1e1cf4126
LouisBluhm <suspiciouspotato@gmail.com>
2016-02-17 13:25:04 +0000

    Updated link to commands

--- Commands.py
+++ Commands.py
@@ -2,7 +2,7 @@ commands = {
 	'admin': 'Contact KRITZWARE if you have any queries or wish to report bugs 4Head',
 	'test': 'test_command recieved',
 	'follow': 'Make sure to follow skowalz on twitch and twitter! twitter.com/skowalz PogChamp',
-	'commands': ', you can view my commands here: https://github.com/LouisBluhm/PyBot MrDestructoid',
+	'commands': ', you can view my commands here: https://github.com/LouisBluhm/PyBot#readme MrDestructoid',
 	'twitter': 'Make sure to follow Skowalz on twitter for the latest updates! twitter.com/skowalz PogChamp',
 	'discord': 'Join the discord to chat with other viewers! https://discord.gg/0dlGiuDScFukCO8y HeyGuys'
 }
