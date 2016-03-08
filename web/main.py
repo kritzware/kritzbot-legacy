ed5230a2b946a7a747c287281fe68bcc40325e8e
LouisBluhm <suspiciouspotato@gmail.com>
2016-03-08 23:29:34 +0000

    Created flask website, added templates, static directories. Added index, commands, 404 pages.

---
+++ web/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index_page():
+	return render_template('index.html')
+
+@app.route('/commands')
+def commands_page():
+	return render_template('commands.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+if __name__ == '__main__':
+	app.run(debug=True)
