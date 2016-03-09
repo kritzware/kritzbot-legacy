3b4fcc59facf14640d7ae7b8a0b85d8c149bacc9
LouisBluhm <suspiciouspotato@gmail.com>
2016-03-09 00:21:40 +0000

    Set up control panel, admin, stats pages. Added temp login function.

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,9 +9,27 @@ def index_page():
 def commands_page():
 	return render_template('commands.html')
 
+@app.route('/stats')
+def stats_page():
+	return render_template('stats.html')
+
+@app.route('/admin')
+def admin_page():
+	return render_template('admin.html')
+
+@app.route('/control-panel', methods=['GET', 'POST'])
+def control_panel_page():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			return redirect(url_for('admin_page'))
+	return render_template('control-panel.html', error=error)
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', debug=True)
